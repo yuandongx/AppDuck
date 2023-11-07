fb415c499fac18602cb0e8727ef10c9ba1a003b8
yuandongx <yuandongx@126.com>
2023-11-07 19:06:38 +0800

    U

--- src/components/layouts/BaseHeader.vue
+++ src/components/layouts/BaseHeader.vue
@@ -4,21 +4,15 @@ import { toggleDark } from "~/composables";
 
 <template>
   <el-menu class="el-menu-demo" mode="horizontal">
-    <el-menu-item index="1">Element Plus</el-menu-item>
+    <el-menu-item index="1">主页面</el-menu-item>
     <el-sub-menu index="2">
-      <template #title>Workspace</template>
-      <el-menu-item index="2-1">item one</el-menu-item>
-      <el-menu-item index="2-2">item two</el-menu-item>
-      <el-menu-item index="2-3">item three</el-menu-item>
-      <el-sub-menu index="2-4">
-        <template #title>item four</template>
-        <el-menu-item index="2-4-1">item one</el-menu-item>
-        <el-menu-item index="2-4-2">item two</el-menu-item>
-        <el-menu-item index="2-4-3">item three</el-menu-item>
-      </el-sub-menu>
+      <template #title>工作空间</template>
+      <el-menu-item index="2-1">日报</el-menu-item>
+      <el-menu-item index="2-2">周报</el-menu-item>
+      <el-menu-item index="2-3">计划</el-menu-item>
     </el-sub-menu>
-    <el-menu-item index="3" disabled>Info</el-menu-item>
-    <el-menu-item index="4">Orders</el-menu-item>
+    <el-menu-item index="3">证券行情</el-menu-item>
+    <el-menu-item index="4">里程碑</el-menu-item>
     <el-menu-item h="full" @click="toggleDark()">
       <button
         class="border-none w-full bg-transparent cursor-pointer"
