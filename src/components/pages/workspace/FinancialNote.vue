d8bfbc83f48a1e0f465b863297e1dc8847f1eada
yuandongx <yuandongx@126.com>
2023-11-24 09:38:20 +0800

    U

--- src/components/pages/workspace/FinancialNote.vue
+++ src/components/pages/workspace/FinancialNote.vue
@@ -1,43 +1,54 @@
-
 <template>
-    <div class="header-tool">
-        <el-button link bg>记一笔</el-button>
-    </div>
-    <el-divider />
-    <el-table height="80vh">
-        <el-table-column label="业务" prop="date" align="center" />
-        <el-table-column label="企业名称" prop="date" align="center" />
-        <el-table-column label="发票信息" align="center">
-            <el-table-column label="种类" prop="date" align="center" />
-            <el-table-column label="日期" prop="date" align="center" />
-            <el-table-column label="金额" prop="date" align="center" />
-            <el-table-column label="差额" prop="date" align="center" />
-            <el-table-column label="手续费" prop="date" align="center" />
-        </el-table-column>
-        <el-table-column label="对方付款" align="center">
-            <el-table-column label="日期" prop="date" align="center" />
-            <el-table-column label="金额" prop="date" align="center" />
-        </el-table-column>
-        <el-table-column label="劳务费" align="center">
-            <el-table-column label="支付日期" prop="date" align="center" />
-            <el-table-column label="支付金额" prop="date" align="center" />
-        </el-table-column>
-        <el-table-column label="备注" prop="date" align="center" />
-        <el-table-column label="操作" prop="date" align="center" />
-    </el-table>
+  <div class="header-tool">
+    <el-button link bg @click="() => onShowInput(true)">记一笔</el-button>
+    <input-form
+      :input-visible="showInput"
+      @change:visible="() => onShowInput(false)"
+    />
+  </div>
+  <el-divider />
+  <el-table height="80vh">
+    <el-table-column label="业务" prop="date" align="center" />
+    <el-table-column label="企业名称" prop="date" align="center" />
+    <el-table-column label="发票信息" align="center">
+      <el-table-column label="种类" prop="date" align="center" />
+      <el-table-column label="日期" prop="date" align="center" />
+      <el-table-column label="金额" prop="date" align="center" />
+      <el-table-column label="差额" prop="date" align="center" />
+      <el-table-column label="手续费" prop="date" align="center" />
+    </el-table-column>
+    <el-table-column label="对方付款" align="center">
+      <el-table-column label="日期" prop="date" align="center" />
+      <el-table-column label="金额" prop="date" align="center" />
+    </el-table-column>
+    <el-table-column label="劳务费" align="center">
+      <el-table-column label="支付日期" prop="date" align="center" />
+      <el-table-column label="支付金额" prop="date" align="center" />
+    </el-table-column>
+    <el-table-column label="备注" prop="date" align="center" />
+    <el-table-column label="操作" prop="date" align="center" />
+  </el-table>
 </template>
+<script setup lang="ts">
+import { ref } from "vue";
 
+const showInput = ref<boolean>(false);
+const onShowInput = (flag: boolean) => {
+  showInput.value = flag;
+  console.log(showInput);
+};
+</script>
 <style scoped>
 .header-tool {
-    text-align: left;
-    padding-left: 5px;
+  text-align: left;
+  padding-left: 5px;
 }
 
-.header-tool+.ep-divider--horizontal {
-    margin: 8px 0;
+.header-tool + .ep-divider--horizontal {
+  margin: 8px 0;
 }
 
 .header-tool .ep-button {
-    padding: 10px 10px;
+  padding: 10px 10px;
 }
 </style>
---
+++ src/components/pages/workspace/InputForm.vue
@@ -0,0 +1,55 @@
+<template>
+  <el-dialog :model-value="inputVisible" @closed="$emit('change:visible')">
+    <el-form v-model="formData">
+      <el-form-item label="业务部门">
+        <el-select
+          multiple
+          filterable
+          allow-create
+          default-first-option
+          :reserve-keyword="false"
+          placeholder="Choose tags for your article"
+          v-model="formData.BusinessDepartment"
+        >
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          />
+        </el-select>
+      </el-form-item>
+    </el-form>
+  </el-dialog>
+</template>
+<script lang="ts" setup>
+import { reactive, ref, defineEmits } from "vue";
+import { FinaNote } from "./types";
+interface Props {
+  inputVisible: boolean;
+}
+interface Option {
+  value: string;
+  label: string;
+}
+const showInput = defineEmits(["change:visible"]);
+const options = ref<Array<Option>>([{ value: "1", label: "外包派遣事业部" }]);
+const formData = reactive<FinaNote>({
+  Id: 0,
+  BusinessDepartment: "",
+  CompanyName: "",
+  InvoiceType: "",
+  InvoiceDate: "",
+  InvoiceAmount: 0,
+  DiffBetweenAmount: 0,
+  ServiceChange: 0,
+  PaymentDate: "",
+  PaymentAmount: 0,
+  PaymentDateOfLabor: "",
+  PaymentDateOfAmount: 0,
+});
+const func = () => {
+  console.log("---func->");
+};
+const props = defineProps<Props>();
+</script>
