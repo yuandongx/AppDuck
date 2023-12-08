493e98214f4bb8e26fa7f140b8ee2e47ef3738d9
yuandongx <yuandongx@126.com>
2023-12-08 20:33:54 +0800

    U

--- src/components/pages/workspace/Upload.vue
+++ src/components/pages/workspace/Upload.vue
@@ -10,16 +10,22 @@
       action="/apiV1/workspace/upload"
       :limit="1"
       :on-exceed="handleExceed"
+      :on-success="handleSuccess"
+      :on-change="handleChange"
       drag
+      :multiple="false"
     >
       <el-icon class="el-icon--upload"><upload-filled /></el-icon>
       <div class="el-upload__text">拖放文件到这里 <em>或点击上传</em></div>
     </el-upload>
+    <el-checkbox-group  v-model="selects">
+      <el-checkbox v-for="item in labels" :label="item" :key="item" />
+    </el-checkbox-group >
   </el-dialog>
 </template>
 <script setup lang="ts">
 import { UploadFilled } from "@element-plus/icons-vue";
-import { ref } from "vue";
+import {   ref } from "vue";
 import { genFileId } from "element-plus";
 import type {
   UploadFile,
@@ -28,7 +34,9 @@ import type {
   UploadProps,
   UploadRawFile,
 } from "element-plus";
-
+let fileName = '';
+const selects = ref<Array<string>>([]);
+const labels = ref<Array<string>>([]);
 defineEmits(["change:visible"]);
 defineProps({
   visibleValue: Boolean,
@@ -41,16 +49,34 @@ const handleExceed: UploadProps["onExceed"] = (files) => {
   file.uid = genFileId();
   upload.value!.handleStart(file);
 };
+const update = (response: any): string[] => {
+  const list: string[] = [];
+  for (let file in response) {
+    fileName = file;
+    for(let sheet in response[file] ){
+      list.push(response[file][sheet]);
+    }
+  }
+  return list
+}
 const handleSuccess: UploadProps["onSuccess"] = (
   response: any,
   uploadFile: UploadFile,
   uploadFiles: UploadFiles
-) => {};
+) => {
+  if (uploadFile.response) labels.value =  [...update(uploadFile.response)];
+};
 const handleError: UploadProps["onError"] = (
   response: any,
   uploadFile: UploadFile,
   uploadFiles: UploadFiles
 ) => {};
+const handleChange: UploadProps["onChange"] = (
+  uploadFile: UploadFile,
+  uploadFiles: UploadFiles
+) => {
+  if (uploadFile.response) labels.value = [...update(uploadFile.response)];
+};
 </script>
 <style scoped>
 .upload-demo {
